df5b740aa5902143b8df70e822fb0a099a193884
Thomas Vercamer <thomas.vercamer@gmail.com>
2022-05-22 09:43:54 +0200

    Implement loader on init and new quote events

--- script.js
+++ script.js
@@ -8,12 +8,14 @@
     const quoteAuthor = document.getElementById("author");
     const twitterBtn = document.getElementById("twitter");
     const newQuoteBtn = document.getElementById("new-quote");
+    const loader = document.getElementById("loader");
 
     // ---------- Functions
     async function getQuotes() {
         // https://type.fit/api/quotes
         const apiURL = "https://type.fit/api/quotes";
         try {
+            startLoading(); // We call this here and in getRandomQuote (once for the initial load, once for the "new quote" functionality
             if(quotes.length > 0) return false;
             const response = await fetch(apiURL);
             quotes = await response.json(); // Use our "global" variable quotes
@@ -24,6 +26,7 @@
     }
 
     function getRandomQuote() {
+        startLoading(); // We call this here and in getQuotes (once for the initial load, once for the "new quote" functionality
         // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
         const randomIndex = Math.floor(Math.random() * Math.floor(quotes.length))
         const randomQuote = quotes[randomIndex];
@@ -37,6 +40,8 @@
         quote.text.length > 50
             ? quoteText.classList.add("long-quote")
             : quoteText.classList.remove("long-quote");
+
+        completeLoading();
     }
 
     function tweetQuote(){
@@ -45,6 +50,16 @@
         const author = quoteAuthor.textContent;
         const twitterUrl = `https://twitter.com/intent/tweet?text=${text} - ${author}`;
         window.open(twitterUrl, "_blank");
+    }
+
+    function startLoading(){
+        loader.hidden = false;
+        quoteContainer.hidden = true;
+    }
+
+    function completeLoading(){
+        loader.hidden = true;
+        quoteContainer.hidden = false;
     }
 
     // ---------- Event Handlers
